6655c6c072eaa74bfa9baf16142ccdde0265f01a
shin-hama <htysivista@gmail.com>
2022-10-12 13:22:00 +0900

    FEAT: Add logout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,20 +7,29 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGear } from '@fortawesome/free-solid-svg-icons'
+import { useUser } from '../contexts/UserAuthorizationProvider'
 
 type Props = {
   children?: React.ReactNode
 }
 const Layout: React.FC<Props> = ({ children }) => {
+  const [user, auth] = useUser()
   return (
     <>
       <AppBar>
         <Toolbar>
           <Typography variant="h4">Reminder Note</Typography>
           <div style={{ flexGrow: 1 }} />
-          <a href="https://api.notion.com/v1/oauth/authorize?client_id=4fdc875a-a164-414f-a321-97bd93f0fe43&response_type=code">
-            <p>login</p>
-          </a>
+          {user ? (
+            <>
+              <p>User: {user.uid}</p>
+              <button onClick={auth.signOut}>logout</button>
+            </>
+          ) : (
+            <a href="https://api.notion.com/v1/oauth/authorize?client_id=4fdc875a-a164-414f-a321-97bd93f0fe43&response_type=code">
+              <p>login</p>
+            </a>
+          )}
           <IconButton component="a" href="/settings">
             <SvgIcon>
               <FontAwesomeIcon icon={faGear} />
--- src/pages/api/notion/notion.ts
+++ src/pages/api/notion/notion.ts
@@ -1,3 +1,7 @@
 import { Client } from '@notionhq/client'
 
 export const notion = new Client({ auth: process.env.NOTION_TOKEN })
+
+export const createClient = (token: string) => {
+  return new Client({ auth: token })
+}
