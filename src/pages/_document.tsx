8a19ffbac2a6f502c42653d858cd20365da76e82
shin-hama <htysivista@gmail.com>
2022-10-19 08:29:31 +0900

    FEAT: Add meta description

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ja">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="ランダムにノートを振り返る「Reminder Note」" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
