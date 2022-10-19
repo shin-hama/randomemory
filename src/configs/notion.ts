92c167583684a490e657ac255e7aad30ad961afb
shin-hama <htysivista@gmail.com>
2022-10-19 11:22:23 +0900

    FEAT: Use different redirect uri on development

--- src/configs/notion.ts
+++ src/configs/notion.ts
@@ -2,4 +2,6 @@ const clientId = process.env.NEXT_PUBLIC_NOTION_CLIENT_ID
 if (!clientId) {
   throw new Error('NEXT_PUBLIC_NOTION_CLIENT_ID is not defined')
 }
-export const NOTION_LOGIN_URL = `https://api.notion.com/v1/oauth/authorize?client_id=${clientId}&response_type=code`
+const redirect =
+  process.env.NODE_ENV === 'development' ? '&redirect_uri=http://localhost:3000/login/notion' : ''
+export const NOTION_LOGIN_URL = `https://api.notion.com/v1/oauth/authorize?client_id=${clientId}&response_type=code${redirect}`
