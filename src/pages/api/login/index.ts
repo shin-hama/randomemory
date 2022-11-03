07cbc809140614bb4f6187affb29d6bd87932d86
shin-hama <htysivista@gmail.com>
2022-11-04 06:30:42 +0900

    FEAT: Check access token when login

--- src/pages/api/login/index.ts
+++ src/pages/api/login/index.ts
@@ -3,14 +3,16 @@ import { createCustomToken } from '../lib/firebaseAuth'
 
 import { getSecrets } from '../lib/secrets'
 import { isValidAccessToken } from '../notion/types'
+import { createUserClient } from '../notion/util'
 
 export type LoginResponse = {
   customToken: string | null
 }
 export default async function handler(req: NextApiRequest, res: NextApiResponse<LoginResponse>) {
   const token = getSecrets(req, 'notion')
+  const notion = await createUserClient(req)
 
-  if (isValidAccessToken(token)) {
+  if (isValidAccessToken(token) && notion) {
     const customToken = await createCustomToken(token.bot_id)
     res.status(200).json({ customToken })
   } else {
