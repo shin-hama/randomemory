c881206b55848e2bd73768aff3b299c83d5d7734
shin-hama <htysivista@gmail.com>
2022-11-13 09:20:28 +0900

    FEAT: Fetch all tweets for user

--- src/pages/api/twitter/[uid]/initialize.ts
+++ src/pages/api/twitter/[uid]/initialize.ts
@@ -3,9 +3,7 @@ import Client from 'twitter-api-sdk'
 
 import { verifyUserToken } from '../../lib/firebaseAuth'
 
-const createUserClient = (req: NextApiRequest) => {
-  return new Client(process.env.TWITTER_BEARER_TOKEN || '')
-}
+const twitter = new Client(process.env.TWITTER_BEARER_TOKEN || '')
 
 export type InitTwitterResponse =
   | {
@@ -19,16 +17,31 @@ export default async function initialize(
   req: NextApiRequest,
   res: NextApiResponse<InitTwitterResponse>
 ) {
-  const client = createUserClient(req)
   const user = await verifyUserToken(req)
 
   const [uid] = [req.query.uid].flat(1)
-  console.log(uid)
-  if (client && user && uid) {
-    const result = await client.tweets.usersIdTweets(uid)
-    // const result = await client.tweets.usersIdTweets()
-    res.status(200).json({ tweets: result, success: true })
+  if (twitter && user && uid) {
+    const tweets = await fetchTweets(uid)
+    res.status(200).json({ tweets, success: true })
   } else {
     res.status(400).json({ success: false })
   }
+}
+
+const fetchTweets = async (uid: string, nextToken?: string): Promise<Array<string>> => {
+  const tweets: Array<string> = []
+  const result = await twitter.tweets.usersIdTweets(uid, {
+    max_results: 100,
+    pagination_token: nextToken,
+  })
+  if (result.data) {
+    tweets.push(...result.data.map((tweet) => tweet.id))
+    if (result.meta?.next_token) {
+      tweets.push(...(await fetchTweets(uid, result.meta.next_token)))
+    }
+  } else if (result.errors) {
+    return []
+  }
+
+  return tweets
 }
