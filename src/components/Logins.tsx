8ff2b6c0357c1ef47b9d3fbc51c3b95129fb6c8d
shin-hama <htysivista@gmail.com>
2022-11-16 06:31:22 +0900

    FEAT: Show twitter login

--- src/components/Logins.tsx
+++ src/components/Logins.tsx
@@ -4,26 +4,41 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
 import NotionIcon from './icons/notion.svg'
+import TwitterIcon from './icons/twitter.svg'
 import { useLogin } from '../hooks/useLogin'
 import { AsyncButton } from './AsyncButton'
+
+type Provider = {
+  name: string
+  icon: React.ReactNode
+}
+const providers: Array<Provider> = [
+  {
+    name: 'Notion',
+    icon: <NotionIcon />,
+  },
+  {
+    name: 'Twitter',
+    icon: <TwitterIcon />,
+  },
+]
 
 const Logins = () => {
   const login = useLogin()
 
   return (
     <Stack alignItems="stretch" spacing={4}>
-      <button onClick={() => login.provider('twitter')}>twitter</button>
       <Typography variant="h5" textAlign="center">
         Sign up Your Account
       </Typography>
-      <AsyncButton variant="outlined" onClick={login.notion}>
-        <Stack direction="row" spacing={2} alignItems="center">
-          <Icon fontSize="large">
-            <NotionIcon />
-          </Icon>
-          <Typography>Continue with Notion</Typography>
-        </Stack>
-      </AsyncButton>
+      {providers.map((provider) => (
+        <AsyncButton key={provider.name} variant="outlined" onClick={login.notion}>
+          <Stack direction="row" spacing={2} alignItems="center">
+            <Icon fontSize="medium">{provider.icon}</Icon>
+            <Typography>Continue with {provider.name}</Typography>
+          </Stack>
+        </AsyncButton>
+      ))}
     </Stack>
   )
 }
